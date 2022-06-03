2d7156f8a411726ca9b6e335280d8b86bbdeb14a
Harsh <harshit30007@gmail.com>
2022-06-03 14:14:43 +0530

    Completed route assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,70 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+router.post('/players', function (req, res) {
+ 
+    //LOGIC WILL COME HERE
+    // if(players.name == req.body.name) {
+    //     console.log("Player already exist")
+    // }else {
+    //     players.push(req.body)
+    // }
+    let n = req.body.name
+    let a = players.find(p => p.name == n)
+    if(a.name != n) {
+        a.push(req.body)
+    } else {
+        console.log("Player already present")
+    }
+    //console.log(req.body)
+    res.send(  { data: a , status: true }  )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.get('/students/:name', function(req, res) {
     let studentName = req.params.name
     console.log(studentName)
@@ -69,5 +133,10 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+
+
+
+
+
 
 module.exports = router;

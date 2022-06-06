3ef2348c9bd14a3806e6197cb48d56d107410f77
harshit073 <harshitsingh@Harshits-MacBook-Air.local>
2022-06-06 10:31:56 +0530

    completed assignment mongosession1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,15 @@
-const UserModel= require("../models/userModel")
+const BookModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
+const createBookUser= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
+    let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
+const getBookUsersData= async function (req, res) {
+    let allUsers= await BookModel.find()
     res.send({msg: allUsers})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createBookUser= createBookUser
+module.exports.getBookUsersData= getBookUsersData
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://harshit073:UESxK1XYbtXr0rbJ@cluster0.f11hm.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://harshit073:swrzc9jWniu8PA86@cluster0.f11hm.mongodb.net/harshit370-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
